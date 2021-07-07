db256edc9cdde884f56ba7ed360dc85b604a00b7
Anand Sinha <63430104+Anand-Sinha@users.noreply.github.com>
2021-07-07 20:21:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TopSection from "./components/TopSection/topsection";
+
+function App() {
+  return (
+    <div>
+      <TopSection />
+      <section className="sectionTwo"></section>
+    </div>
+  );
+}
+
+export default App;
